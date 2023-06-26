085d56766149ebb487030239d9395be31fb4975c
ginafender <123966430+ginafender@users.noreply.github.com>
2023-06-26 18:38:02 -0400

    updated code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,30 +1,51 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+// Fetch the JSON data and process it    
+function bargraphFunction(bargraph) {
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-    console.log(data);
-});
+  d3.json(url).then(function(data) {
+      console.log(data);
 
-let sample_values = Object.values(samples.sample_values);
-let otu_ids = Object.values(samples.otu_ids);
-let otu_labels = Object.values(samples.otu_labels);
+        let samples = data.samples;
+        // let firstsample = samples[0]
+        let filteredData = samples.filter(sam => sam.id === bargraph)[0];
 
-// display default plot
+        let trace1 = {
+            x: filteredData.sample_values.slice(0, 10).reverse(),
+            y: filteredData.otu_ids.slice(0, 10).map(text => `OTU ${text}`).reverse(),
+            type: 'bar',
+            text: filteredData.otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+        };
+
+        let mapdata = [trace1];
+
+        var layout = {
+            title: 'Top 10 OTUs by Individual',
+        };
+
+        Plotly.newPlot('bar', mapdata, layout);
+
+      });
+
+  };
+
 function init() {
-    let data = [{
-        values: sample_values,
-        labels: otu_ids,
-        type: "bar"
-    }];
+  let dropdownMenu = d3.select("#selDataset");
 
-    let layout = {
-        height: 600,
-        width: 800
+  d3.json(url).then(function(data) {
+
+    let names = data.names;
+    names.forEach((eachName) => {dropdownMenu.append("option").text(eachName)
+      .property("value", eachName)}) 
+
+        bargraphFunction(names[0])
+
+      });
     };
 
-    Plotly.newPlot("bar", data, layout);
-}
+init();
+
+function optionChanged(newValue) {
+  bargraphFunction(newValue)
+};
